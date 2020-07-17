858f83a985701fae00bb6aa317062213d0de740e
LeeSangHa <38284431+StrangeLee@users.noreply.github.com>
2020-07-17 15:57:02 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/strange/excuse_me_sir_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.strange.excuse_me_sir_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return Center(
+      child: Text('text'),
+    );
+  }
+}
