080c05b29b7c39eaf835144b2009b9c612ef0c4b
LeeSangHa <38284431+StrangeLee@users.noreply.github.com>
2020-07-18 15:57:36 +0900

    [ADD] TTS Function

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -6,16 +7,41 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final FlutterTts _flutterTts = FlutterTts();
+  TextEditingController _speakController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'tts test'
+        ),
+      ),
       body: _body(),
     );
   }
-
+  
   Widget _body() {
     return Center(
-      child: Text('text'),
+      child: Column(
+        children: [
+          TextField(
+            controller: _speakController,
+            maxLength: 20,
+          ),
+          FlatButton(
+            onPressed: () => _speak(),
+            child: Text('sound'),
+          ),
+        ],
+      ),
     );
+  }
+
+  Future _speak() async {
+    await _flutterTts.setLanguage('ko-KR');
+    await _flutterTts.setSpeechRate(1);
+    await _flutterTts.speak(_speakController.text);
   }
 }
